836a0dcd6adfbbe3e045a8aae849bee6fd62db02
Johnny1023 <Johnny1023@users.noreply.github.com>
2020-01-09 11:22:34 -0700

    20-01-09
    Signed-off-by: Johnny1023 <Johnny1023@users.noreply.github.com>

--- app/src/main/java/com/jophnny/myapplication/MainActivity.java
+++ app/src/main/java/com/jophnny/myapplication/MainActivity.java
@@ -1,8 +1,16 @@
 package com.jophnny.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavController navController = null;
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setDrawerLayout(R.id.drawer_host).build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, navController);
     }
+
+    Button card1 = (Button) findViewById(R.id.card1);
+    Button card2 = (Button) findViewById(R.id.card2);
+    card1.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+    card2.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+
 }
